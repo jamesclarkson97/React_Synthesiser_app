3920e53002bf48df0944664de5150dea65f0bccc
Stephen <stephen_h369@live.co.uk>
2020-09-22 21:11:19 +0100

    css positioning and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import reactLogo from './images/react-logo.png';
 import SynthesiserContainer from './Container/SynthesiserContainer';
 
 function App() {
   
   return (
     <div className="App">
+      <div id="main-header">
+        <img src={reactLogo} alt="react-logo" id="react-logo"/>
+        <h1 id="main-heading">Synth.</h1>
+      </div>
       <SynthesiserContainer/>
     </div>
   );
