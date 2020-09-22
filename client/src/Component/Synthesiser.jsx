998eed7070286f482aafaff2ae44fe00d5c8f231
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-09-22 10:39:40 +0100

    added octave changer to key bindings

--- client/src/Component/Synthesiser.jsx
+++ client/src/Component/Synthesiser.jsx
@@ -10,68 +10,7 @@ class Synthesiser extends Component {
       Tone.start();
     }
 
-    handleKeyDown = (e) => {
-      let upperOctave = Number(this.props.octave) + 1;
-      e = window.event;
-      let k = e.keyCode;
-	    switch(k){
-        default:
-          
-          break;
-        case 75:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`C${upperOctave}`, "8n")
-          break;
-        case 74:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`B${this.props.octave}`, "8n")
-          break;
-        case 85:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`A#${this.props.octave}`, "8n")
-          break;
-        case 72:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`A${this.props.octave}`, "8n")
-          break;
-        case 89:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`G#${this.props.octave}`, "8n")
-          break;
-        case 71:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`G${this.props.octave}`, "8n")
-          break;
-        case 84:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`F#${this.props.octave}`, "8n")
-          break;
-        case 70:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`F${this.props.octave}`, "8n")
-          break;
-        case 68:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`E${this.props.octave}`, "8n")
-          break;
-        case 69:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`D#${this.props.octave}`, "8n")
-          break;
-        case 83:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`D${this.props.octave}`, "8n")
-          break;
-        case 87:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`C#${this.props.octave}`, "8n")
-          break;
-        case 65:
-          Tone.start()
-          this.props.synth1.triggerAttackRelease(`C${this.props.octave}`, "8n")
-          break;
-        }
-    }
+    
     
     handleOnLeave = (e) => {
       this.props.synth1.triggerRelease(e.target.id);
@@ -84,7 +23,7 @@ class Synthesiser extends Component {
     var newOctave = Number(this.props.octave) + 1;
     
     return (
-      <div className="synthesiser-notes" onKeyDown={this.handleKeyDown} tabIndex="0">
+      <div className="synthesiser-notes" >
         <div id={"C" + newOctave} onMouseDown={this.handleOnDown} onMouseUp={this.handleOnLeave} onMouseOut={this.handleOnLeave}className="white-note">C{newOctave}
         </div>
         <div id={"B"+ this.props.octave} onMouseDown={this.handleOnDown} onMouseUp={this.handleOnLeave} onMouseOut={this.handleOnLeave} className="white-note">B{this.props.octave}
--- client/src/Container/SynthesiserContainer.jsx
+++ client/src/Container/SynthesiserContainer.jsx
@@ -15,15 +15,101 @@ class SynthesiserContainer extends Component {
     }
   }
 
+  handleKeyDown = (e) => {
+    let upperOctave = Number(this.state.octave) + 1;
+    e = window.event;
+    let k = e.keyCode;
+    switch(k){
+      default:
+        
+        break;
+      // Octave
+      case 49:
+        this.setState({octave: '1'})
+        document.getElementById("octave-select").value = '1'
+        break;
+      case 50:
+        this.setState({octave: '2'})
+        document.getElementById("octave-select").value = '2'
+        break;
+      case 51:
+        this.setState({octave: '3'})
+        document.getElementById("octave-select").value = '3'
+        break;
+      case 52:
+        this.setState({octave: '4'})
+        document.getElementById("octave-select").value = '4'
+        break;
+      case 53:
+        this.setState({octave: '5'})
+        document.getElementById("octave-select").value = '5'
+        break;
+
+      // Notes
+      case 75:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`C${upperOctave}`, "8n")
+        break;
+      case 74:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`B${this.state.octave}`, "8n")
+        break;
+      case 85:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`A#${this.state.octave}`, "8n")
+        break;
+      case 72:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`A${this.state.octave}`, "8n")
+        break;
+      case 89:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`G#${this.state.octave}`, "8n")
+        break;
+      case 71:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`G${this.state.octave}`, "8n")
+        break;
+      case 84:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`F#${this.state.octave}`, "8n")
+        break;
+      case 70:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`F${this.state.octave}`, "8n")
+        break;
+      case 68:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`E${this.state.octave}`, "8n")
+        break;
+      case 69:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`D#${this.state.octave}`, "8n")
+        break;
+      case 83:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`D${this.state.octave}`, "8n")
+        break;
+      case 87:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`C#${this.state.octave}`, "8n")
+        break;
+      case 65:
+        Tone.start()
+        this.state.synth1.triggerAttackRelease(`C${this.state.octave}`, "8n")
+        break;
+      }
+  }
+
   handleSelect = (e) => this.setState({octave: e.target.value});
 
   render() {
 
       return(
-          <div className="synthesiser-container">
+          <div className="synthesiser-container" onKeyDown={this.handleKeyDown} tabIndex="0">
             <div className="controls-container">
               <h6>Octave</h6>
-              <select defaultValue="3" onChange={this.handleSelect} className="octave-select">
+              <select id="octave-select" defaultValue="3" onChange={this.handleSelect} className="octave-select">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
