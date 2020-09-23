24138d4b79effa1aaf7224ee2e1c2b828a4aca6c
Stephen <stephen_h369@live.co.uk>
2020-09-23 11:51:33 +0100

    MIDI works

--- client/src/Component/MIDIAccess.jsx
+++ client/src/Component/MIDIAccess.jsx
@@ -2,15 +2,8 @@ import React, {Component} from 'react';
 import * as Tone from 'tone';
 
 class MIDIAccess extends Component {
-    constructor() {
-        super();
-        // this.ondeviceInput = args.onDeviceInput
-        
-            // this.props.onDeviceInput appears to be a function from args
-    }
 
     start = () => {
-        console.log("start called")
         return new Promise((resolve, reject) => {
             this._requestAccess().then(access => {
                 this.initialise(access);
@@ -20,46 +13,33 @@ class MIDIAccess extends Component {
     }
 
     _requestAccess = () => {
-        console.log("_requestAccess called")
         return new Promise((resolve, reject) => {
             if (navigator.requestMIDIAccess) {
-                console.log("requestMIDIAccess called")
                 navigator.requestMIDIAccess()
                 .then(resolve)
                 .catch(reject);
             } else {
-                console.log("requestMIDIAccess failed?")
                 reject()
             }
         })
     }
 
     initialise = (access) => {
-        console.log("initialise called. Here is access:", access)
         const devices = access.inputs.values();
         for (let device of devices) this.initialiseDevice(device); 
     }
 
     initialiseDevice = (device) => {
-        console.log("initialiseDevice called. Here is device:", device)
         device.onmidimessage = this.onMessage.bind(this);
     }
 
     onMessage = (message) => {
-        console.log("onMessage called. Here is message:", message)
-        let [_, input, value] = message.data;
-        // originally this.onDeviceInput, but will make it state because I have a hunch
-        this.props.onDeviceInput({input, value})
+        this.props.onDeviceInput(message);
     }
 
-    // componentDidMount() {
-        
-    // }
 
     render() {
-        this.start().then(() => {
-            console.log('STARTED!');
-        }).catch(console.error);
+        this.start().catch(console.error);
         return(
             <>
             </>
---
+++ client/src/Component/MIDIPlay.jsx
@@ -0,0 +1,220 @@
+import React, {Component} from 'react';
+import * as Tone from 'tone';
+import MIDIAccess from './MIDIAccess.jsx';
+
+class MIDIPlay extends Component {
+
+    onDeviceInput = (message) => {
+        Tone.start();
+        message.stopPropagation() 
+        let [onOff, input, value] = message.data;
+        switch(input) {
+         default: 
+         break;
+          case 36:
+            if (onOff === 144) this.props.synth1.triggerAttack('C2');
+            else this.props.synth1.triggerRelease('C2');
+            break;
+          case 37:
+            if (onOff === 144) this.props.synth1.triggerAttack('C#2');
+            else this.props.synth1.triggerRelease('C#2');
+            break;
+          case 38:
+            if (onOff === 144) this.props.synth1.triggerAttack('D2');
+            else this.props.synth1.triggerRelease('D2');
+             break;
+          case 39:
+            if (onOff === 144) this.props.synth1.triggerAttack('D#2');
+            else this.props.synth1.triggerRelease('D#2');
+            break;
+          case 40:
+            if (onOff === 144) this.props.synth1.triggerAttack('E2');
+            else this.props.synth1.triggerRelease('E2');
+          break;
+          case 41:
+            if (onOff === 144) this.props.synth1.triggerAttack('F2');
+            else this.props.synth1.triggerRelease('F2');
+            break;
+          case 42:
+            if (onOff === 144) this.props.synth1.triggerAttack('F#2');
+            else this.props.synth1.triggerRelease('F#2');
+            break;
+          case 43:
+            if (onOff === 144) this.props.synth1.triggerAttack('G2');
+            else this.props.synth1.triggerRelease('G2');
+             break;
+          case 44:
+            if (onOff === 144) this.props.synth1.triggerAttack('G#2');
+            else this.props.synth1.triggerRelease('G#2');
+            break;
+          case 45:
+            if (onOff === 144) this.props.synth1.triggerAttack('A2');
+            else this.props.synth1.triggerRelease('A2');
+          break;
+          case 46:
+            if (onOff === 144) this.props.synth1.triggerAttack('A#2');
+            else this.props.synth1.triggerRelease('A#2');
+            break;
+          case 47:
+            if (onOff === 144) this.props.synth1.triggerAttack('B2');
+            else this.props.synth1.triggerRelease('B2');
+            break;
+          case 48:
+            if (onOff === 144) this.props.synth1.triggerAttack('C3');
+            else this.props.synth1.triggerRelease('C3');
+            break;
+          case 49:
+            if (onOff === 144) this.props.synth1.triggerAttack('C#3');
+            else this.props.synth1.triggerRelease('C#3');
+            break;
+          case 50:
+            if (onOff === 144) this.props.synth1.triggerAttack('D3');
+            else this.props.synth1.triggerRelease('D3');
+            break;
+          case 51:
+            if (onOff === 144) this.props.synth1.triggerAttack('D#3');
+            else this.props.synth1.triggerRelease('D#3');
+            break;
+          case 52:
+            if (onOff === 144) this.props.synth1.triggerAttack('E3');
+            else this.props.synth1.triggerRelease('E3');
+          break;
+          case 53:
+            if (onOff === 144) this.props.synth1.triggerAttack('F3');
+            else this.props.synth1.triggerRelease('F3');
+            break;
+          case 54:
+            if (onOff === 144) this.props.synth1.triggerAttack('F#3');
+            else this.props.synth1.triggerRelease('F#3');
+            break;
+          case 55:
+            if (onOff === 144) this.props.synth1.triggerAttack('G3');
+            else this.props.synth1.triggerRelease('G3');
+             break;
+          case 56:
+            if (onOff === 144) this.props.synth1.triggerAttack('G#3');
+            else this.props.synth1.triggerRelease('G#3');
+            break;
+          case 57:
+            if (onOff === 144) this.props.synth1.triggerAttack('A3');
+            else this.props.synth1.triggerRelease('A3');
+          break;
+          case 58:
+            if (onOff === 144) this.props.synth1.triggerAttack('A#3');
+            else this.props.synth1.triggerRelease('A#3');
+            break;
+          case 59:
+            if (onOff === 144) this.props.synth1.triggerAttack('B3');
+            else this.props.synth1.triggerRelease('B3');
+            break;
+          case 60:
+            if (onOff === 144) this.props.synth1.triggerAttack('C4');
+            else this.props.synth1.triggerRelease('C4');
+            break;
+          case 61:
+            if (onOff === 144) this.props.synth1.triggerAttack('C#4');
+            else this.props.synth1.triggerRelease('C#4');
+            break;
+          case 62:
+            if (onOff === 144) this.props.synth1.triggerAttack('D4');
+            else this.props.synth1.triggerRelease('D4');
+            break;
+          case 63:
+            if (onOff === 144) this.props.synth1.triggerAttack('D#4');
+            else this.props.synth1.triggerRelease('D#4');
+            break;
+          case 64:
+            if (onOff === 144) this.props.synth1.triggerAttack('E4');
+            else this.props.synth1.triggerRelease('E4');
+            break;
+          case 65:
+            if (onOff === 144) this.props.synth1.triggerAttack('F4');
+            else this.props.synth1.triggerRelease('F4');
+            break;
+          case 66:
+            if (onOff === 144) this.props.synth1.triggerAttack('F#4');
+            else this.props.synth1.triggerRelease('F#4');
+            break;
+          case 67:
+            if (onOff === 144) this.props.synth1.triggerAttack('G4');
+            else this.props.synth1.triggerRelease('G4');
+            break;
+          case 68:
+            if (onOff === 144) this.props.synth1.triggerAttack('G#4');
+            else this.props.synth1.triggerRelease('G#4');
+            break;
+          case 69:
+            if (onOff === 144) this.props.synth1.triggerAttack('A4');
+            else this.props.synth1.triggerRelease('A4');
+            break;
+          case 70:
+            if (onOff === 144) this.props.synth1.triggerAttack('A#4');
+            else this.props.synth1.triggerRelease('A#4');
+            break;
+          case 71:
+            if (onOff === 144) this.props.synth1.triggerAttack('B4');
+            else this.props.synth1.triggerRelease('B4');
+            break;
+          case 72:
+            if (onOff === 144) this.props.synth1.triggerAttack('C5');
+            else this.props.synth1.triggerRelease('C5');
+            break;
+          case 73:
+            if (onOff === 144) this.props.synth1.triggerAttack('C#5');
+            else this.props.synth1.triggerRelease('C#5');
+            break;
+          case 74:
+            if (onOff === 144) this.props.synth1.triggerAttack('D5');
+            else this.props.synth1.triggerRelease('D5');
+            break;
+          case 75:
+            if (onOff === 144) this.props.synth1.triggerAttack('D#5');
+            else this.props.synth1.triggerRelease('D#5');
+            break;
+          case 76:
+            if (onOff === 144) this.props.synth1.triggerAttack('E5');
+            else this.props.synth1.triggerRelease('E5');
+            break;
+          case 77:
+            if (onOff === 144) this.props.synth1.triggerAttack('F5');
+            else this.props.synth1.triggerRelease('F5');
+            break;
+          case 78:
+            if (onOff === 144) this.props.synth1.triggerAttack('F#5');
+            else this.props.synth1.triggerRelease('F#5');
+            break;
+          case 79:
+            if (onOff === 144) this.props.synth1.triggerAttack('G5');
+            else this.props.synth1.triggerRelease('G5');
+            break;
+          case 80:
+            if (onOff === 144) this.props.synth1.triggerAttack('G#5');
+            else this.props.synth1.triggerRelease('G#5');
+            break;
+          case 81:
+            if (onOff === 144) this.props.synth1.triggerAttack('A5');
+            else this.props.synth1.triggerRelease('A5');
+            break;
+          case 82:
+            if (onOff === 144) this.props.synth1.triggerAttack('A#5');
+            else this.props.synth1.triggerRelease('A#5');
+            break;
+          case 83:
+            if (onOff === 144) this.props.synth1.triggerAttack('B5');
+            else this.props.synth1.triggerRelease('B5');
+            break;
+          case 84:
+            if (onOff === 144) this.props.synth1.triggerAttack('C6');
+            else this.props.synth1.triggerRelease('C6');
+            break;
+        }
+    }
+
+    render() {
+        return(
+            <MIDIAccess onDeviceInput={this.onDeviceInput}/>
+        )
+    }
+}
+
+export default MIDIPlay;
