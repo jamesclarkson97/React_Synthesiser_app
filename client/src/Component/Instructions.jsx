a5851f287ccaabf7d0d631fdf95bf0e26e627d3a
Stephen <stephen_h369@live.co.uk>
2020-09-23 15:44:17 +0100

    instructions rewording and colour

--- client/src/Component/Instructions.jsx
+++ client/src/Component/Instructions.jsx
@@ -5,11 +5,11 @@ const Instructions = () => {
     
     return(
         <div id="instructions">
-        <h5>Welcome to React Synth.</h5>
-        <p>Use the global controls to alter the sound of all synths in the app.</p>
+        <h4>Welcome to React Synth.</h4>
+        <p>Use the control panel on the left to alter the sound of all synths in the app.</p>
         <p>Press the virtual keys on screen or use the following keys on your keyboard:</p>
-        <p>A, W, S, E, D, F, T, G, Y, H, U, J and K (They assend in chromatic order).</p>
-        <p>You can use the 1, 2, 3, 4 and 5 to select octave quickly.</p>
+        <p>A, W, S, E, D, F, T, G, Y, H, U, J and K (in ascending order).</p>
+        <p>You can press 1, 2, 3, 4 and 5 to select octave quickly.</p>
         <p>Click any box in the Step-Sequencer grid, and press play and watch it loop at a tempo of your choice! You can then save your favourite sequences, and load them in again later!</p>
         </div>
     )
